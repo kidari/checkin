db8b35111758b79d2deb781fb112327cf411857b
kidari <kidari@qq.com>
2020-10-19 16:55:34 +0800

    修改文件内容

--- checkin.py
+++ checkin.py
@@ -1,5 +1,5 @@
 import os
-import re
+import re,time
 from DecryptLogin import login
 from DecryptLogin.platforms.music163 import Cracker
 import smtplib
@@ -48,16 +48,16 @@ class NeteaseSignin():
 
 '''run'''
 if __name__ == '__main__':
-    # username = os.environ["NETEASE_USERNAME"]
-    # password = os.environ["NETEASE_PASSWORD"]
-    # sign_in = NeteaseSignin(username=username, password=password)
-    # sign_in.run()
+    username = os.environ["NETEASE_USERNAME"]
+    password = os.environ["NETEASE_PASSWORD"]
+    sign_in = NeteaseSignin(username=username, password=password)
+    sign_in.run()
 
     #设置服务器所需信息
     #163邮箱服务器地址
     mail_host = 'smtp.126.com'  
     #163用户名
-    mail_user = 'sunduo88772922'  
+    mail_user = os.environ["NETEASE_USERNAME"] 
     #密码(部分邮箱为授权码) 
     mail_pass = os.environ["SENDER_KEY"]  
     #邮件发送方邮箱地址
@@ -73,15 +73,15 @@ if __name__ == '__main__':
     #发送方信息
     message['From'] = sender 
     #接受方信息     
-    message['To'] = receivers[0]  
-
+    message['To'] = receivers[0]
+    message.set_payload('exec python file==='+time.strftime('%H:%M:%S', time.localtime()))
     #登录并发送邮件
     try:
-        # smtpObj = smtplib.SMTP() 
-        #连接到服务器
-        # smtpObj.connect(mail_host,25)
+        smtpObj = smtplib.SMTP() 
+        # 连接到服务器
+        smtpObj.connect(mail_host,25)
         #######替换为########
-        smtpObj = smtplib.SMTP_SSL(mail_host)
+        # smtpObj = smtplib.SMTP_SSL(mail_host)
         #登录到服务器
         smtpObj.login(mail_user,mail_pass) 
         #发送
